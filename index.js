40bcd769a39fc5c7453bcd83e96890161cd0d8db
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-12-05 20:45:20 +0530

    turn date number to string

--- index.js
+++ index.js
@@ -13,5 +13,23 @@ function isPalindrome(reverseStringChar) {
     return false;
   }
 }
-console.log(isPalindrome("hellow"));
-console.log(isPalindrome("racecar"));
+
+const date = {
+  day: 6,
+  month: 9,
+  year: 2020,
+};
+
+function convertDateNumberToString(date) {
+  let stringFormateDate = { day: "", month: "", year: "" };
+  date.day < 10
+    ? (stringFormateDate.day = "0" + date.day)
+    : (stringFormateDate.day = date.day.toString());
+  date.month < 10
+    ? (stringFormateDate.month = "0" + date.month)
+    : (stringFormateDate.month = date.month.toString());
+  stringFormateDate.year = date.year.toString();
+  return stringFormateDate;
+}
+
+console.log(convertDateNumberToString(date));
