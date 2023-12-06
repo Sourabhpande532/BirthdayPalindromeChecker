91c85c86521be5f806c1b53a16768d05f96aab2a
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-12-06 20:44:48 +0530

    Exercise - Done! for convert Date & review it

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function reverseString(string) {
   return concateReverseListOfChar;
 }
 
+/* check isPalindrome or not */
 function isPalindrome(reverseStringChar) {
   let isPalindromeString = reverseString(reverseStringChar);
   if (isPalindromeString === reverseStringChar) {
@@ -13,10 +14,11 @@ function isPalindrome(reverseStringChar) {
     return false;
   }
 }
+console.log(isPalindrome("racecar"));
 
 const date = {
-  day: 6,
-  month: 9,
+  day: 20,
+  month: 2,
   year: 2020,
 };
 
@@ -33,3 +35,30 @@ function convertDateNumberToString(date) {
 }
 
 console.log(convertDateNumberToString(date));
+
+function getAllDateFormate(date) {
+  let dateString = convertDateNumberToString(date);
+  let ddmmyyyy = dateString.day + dateString.month + dateString.year;
+  let mmddyyyy = dateString.month + dateString.day + dateString.year;
+  let yyyymmdd = dateString.year + dateString.year + dateString.day;
+  let ddmmyy = dateString.day + dateString.month + dateString.year.slice(-2);
+  let mmddyy = dateString.month + dateString.day + dateString.year.slice(-2);
+  let yymmdd = dateString.year.slice(-2) + dateString.month + dateString.day;
+  return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
+}
+console.log(getAllDateFormate(date));
+
+
+function checkIsPalindromeForAllDateFormats(date){
+let listOfPalindromDate = getAllDateFormate(date);
+let isThis = false;
+for(let i = 0; i<listOfPalindromDate.length; i++){
+  if(isPalindrome(listOfPalindromDate[i])){
+    isThis = true;
+    break;
+  }
+}
+return isThis;
+}
+
+console.log(checkIsPalindromeForAllDateFormats(date));
